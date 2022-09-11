c701240faddc7ec3507c8a85cc782a767b764d7d
Ffarzan11 <farhikhtafarzan@gmail.com>
2022-09-11 18:37:23 -0400

    resolved merge conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Header from '../components/header'
 
@@ -8,12 +7,6 @@ const Home = () => {
     <Header title = 'Chat Rooms'/>
     </div>
   )
-=======
-import React from "react";
-
-const Home = () => {
-  return <div>hello</div> 
->>>>>>> de12d76e6482793e1de744e936e367c3c0c9da0b
 }
 
 export default Home
