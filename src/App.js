64380b7b8a1d66370a9b8b3a316805355cc41cf1
Ffarzan11 <farhikhtafarzan@gmail.com>
2022-09-10 21:42:35 -0400

    fixed error in app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
-=======
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
@@ -20,7 +11,6 @@ function App() {
         <Route index element={<Home/> }/>
       </Route>
     </Routes>
->>>>>>> 4df31fe6b80b5eb34b0a67db23defc10ef34dca5
   );
 }
 
