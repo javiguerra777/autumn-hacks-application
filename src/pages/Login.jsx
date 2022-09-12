4d671a6bfd0eff4d3fb81d5029dd2114aba6ebf2
Javier Guerra <javier.guerra1001@gmail.com>
2022-09-11 18:22:13 -0700

    feat: login page and signup page created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import '../styles/register.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function loginToAccount(e) {
+    e.preventDefault();
+    navigate('/dashboard');
+  }
   return (
-    <div>Login</div>
+    <main id="register-page">
+      <header>
+      <h1>Log in to account</h1>
+      </header>
+      <form onSubmit={loginToAccount}>        
+        <label htmlFor="email">
+          <p id="first">Email:</p>
+        <input type="email" id="email" value={email} onChange={({target: {value}}) => setEmail(value)} />
+        </label>
+        <label htmlFor="password">
+          <p>Password:</p>
+          <input type="password" id="password" value={password} onChange={({target: {value}}) => setPassword(value) } />
+        </label>
+        <button className='submit-btn' type="submit">Log In</button>
+      </form>
+      <section id="options">
+        <NavLink to="/signup">Sign up for account</NavLink>
+      </section>
+    </main>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function registerAccount(e) {
+    e.preventDefault();
+    navigate('/dashboard');
+  }
   return (
-    <div>Signup</div>
+    <main id="register-page">
+      <header>
+        <h1>Sign up for an account</h1>
+      </header>
+      <form onSubmit={registerAccount}>
+        <label htmlFor="name">
+          <p id="first">Name:</p>
+        <input type="text" id="email" value={name} onChange={({target: {value}}) => setName(value)} />
+        </label>
+        <label htmlFor="email">
+          <p>Email:</p>
+        <input type="email" id="email" value={email} onChange={({target: {value}}) => setEmail(value)} />
+        </label>
+        <label htmlFor="password">
+          <p>Password:</p>
+          <input type="password" id="password" value={password} onChange={({target: {value}}) => setPassword(value) } />
+        </label>
+        <button className='submit-btn' type="submit">Register Account</button>
+      </form>
+    </main>
   )
 }
 
-export default Signup
+export default Signup;
