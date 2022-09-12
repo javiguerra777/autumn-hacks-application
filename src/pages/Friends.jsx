71a42a41dc72ea221737bdf25dd27fdac047c55c
Ffarzan11 <farhikhtafarzan@gmail.com>
2022-09-11 22:09:20 -0400

    friends page styled

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import '../styles/Friends.css';
 
 const Friends = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <header>
+      <div class = "navigation">
+        <Navbar />
+      </div>
+      <div className='title'>
+        Friends
+      </div>
+      
+      <div class = "friend-wrap">
+        <button type = "button" className='add-friend'>Add Friend</button>
+      <p class = "friend-area">
+        <p><label className='labels'>Name</label>
+        <input type="text" /></p>
+        <p><label className='labels'>Email </label>
+        <input type="email" /></p>
+        <p><label className='labels'>Phone</label>
+        <input type="number" /></p>
+        
+        </p>
+        <p class = "show-friend"></p>
+      </div>
+    </header>
+
   )
 }
 
